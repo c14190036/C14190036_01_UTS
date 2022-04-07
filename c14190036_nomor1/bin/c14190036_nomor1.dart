406f959221aaac8c4cd5b445da457a3df4e2d173
c14190036 <100283424+c14190036@users.noreply.github.com>
2022-04-07 15:05:01 +0700

    Update c14190036_nomor1.dart
    update sementara

--- c14190036_nomor1/bin/c14190036_nomor1.dart
+++ c14190036_nomor1/bin/c14190036_nomor1.dart
@@ -1,3 +1,82 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  stdout.write("Masukan Kata : ");
+  String? kata = stdin.readLineSync();
+
+  stdout.write("Masukan Jumlah : ");
+  String? jumlah = stdin.readLineSync();
+  int angka = int.parse(jumlah!);
+
+  if (angka % 2 == 1) {
+    int col = 0;
+    int temp = 0;
+    int count = 0;
+    var list = [];
+
+    //sisi atas
+    for (int i = 1; i <= (angka / 2).floor() + 1; i++) {
+      for (int j = 1; j <= angka; j++) {
+        if (j == (angka / 2).floor() - i + 2 && col == 0) {
+          if (count == 9) {
+            count = 0;
+          }
+          stdout.write(count + 1);
+          col = i * 2;
+        } else if (col != 2 && temp < col - 2) {
+          temp++;
+          if (temp % 2 == 0) {
+            if (count == 9) {
+              count = 0;
+            }
+            stdout.write(count + 1);
+          } else if (temp % 2 == 1) {
+            stdout.write(" ");
+          }
+        } else {
+          stdout.write(" "); //*
+        }
+      }
+      count++;
+      temp = 0;
+      col = 0;
+      print("");
+    }
+
+    //sisi bawah
+    col = 0;
+    temp = 0;
+    for (int i = (angka / 2).floor(); i >= 1; i--) {
+      for (int j = 1; j <= angka; j++) {
+        if (j == (angka / 2).floor() - i + 2 && col == 0) {
+          if (count == 9) {
+            count = 0;
+          }
+          stdout.write(count + 1);
+          col = i * 2;
+        } else if (col != 2 && temp < col - 2) {
+          temp++;
+
+          if (temp % 2 == 0) {
+            if (count == 9) {
+              count = 0;
+            }
+            stdout.write(count + 1);
+          } else {
+            stdout.write(" ");
+          }
+        } else {
+          stdout.write(" ");
+        }
+      }
+      count++;
+      temp = 0;
+      col = 0;
+
+      print("");
+    }
+  } else {
+    int count = 0;
+    int count2 = angka;
+  }
 }
